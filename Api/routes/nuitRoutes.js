f59d7785cd849c1ef8b70c49aab8ef7be893a1a8
Svetlana Sultanyan <svetlannasultanyan@gmail.com>
2026-06-30 15:47:07 +0200

    Nuits routes

--- Api/routes/nuitRoutes.js
+++ Api/routes/nuitRoutes.js
@@ -1,10 +1,15 @@
+// nuitRoutes.js
 import express from 'express';
-// Corrected: Remove the duplicate 'getStats'
-import { getNuitData, getStats } from "../controllers/nuitController.js";
+// Assurez-vous d'importer updateNuit ici
+import { getNuitData, getStats, getAllNuitEtude, updateNuit } from "../controllers/nuitController.js";
 
 const router = express.Router();
 
+router.get('/', getAllNuitEtude);
 router.get('/:id/run', getNuitData);
 router.get('/:id/stats', getStats);
 
+// Ajoutez cette ligne pour rendre la route accessible :
+
+router.put('/:id/update', updateNuit);
 export default router;
