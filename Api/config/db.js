30119441e2e6647607efd99fd90ef55933993d0a
Sophian Sefiani <sophiansimplon@gmail.com>
2026-07-02 13:46:27 +0200

    popup enregistré commentaire

--- Api/config/db.js
+++ Api/config/db.js
@@ -3,9 +3,9 @@ import mysql from 'mysql2/promise';
 export const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    port: process.env.DB_PORT || 3333,
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || "clinique2nuitsv2",
+    port: process.env.DB_PORT || 3306,
+    password: process.env.DB_PASSWORD || 'Kiljaeden34!',
+    database: process.env.DB_NAME || "clinique",
     waitForConnections: true,
     connectionLimit: 10
 });
